992fba3b5b315cc56338ce867e48a91902adf984
Tim Page <tim.page@warwick.ac.uk>
2022-04-14 14:35:38 +0100

    improved transition score calculation

--- src/main/java/com/timpage/app/Transition.java
+++ src/main/java/com/timpage/app/Transition.java
@@ -1,5 +1,7 @@
 package com.timpage.app;
 
+import java.util.Enumeration;
+
 public class Transition implements Comparable<Transition> {
 
     private ChordMap srcMap;
@@ -25,8 +27,53 @@ public class Transition implements Comparable<Transition> {
     public float calculateTransitionScore(ChordMap srcMap) {
         transitionScore = 0;
         float neckMovement = Math.abs(dstMap.getAverageFret()-srcMap.getAverageFret());
-
-        transitionScore = neckMovement + srcMap.getScore() + dstMap.getScore();
+        int matchingFingerings = 2*dstMap.size();
+        Enumeration<Integer> e = srcMap.getFretting().keys();
+        while (e.hasMoreElements()) {
+            int s = e.nextElement();
+            int f = srcMap.getFretting().get(s);
+            if (dstMap.getFretting().containsKey(s)) {
+                int dstFret = dstMap.getFretting().get(s);
+                if (f == dstFret) {
+                    matchingFingerings-=2;
+                }
+            }
+        }
+        int constDiff = 0;
+        int stringMatches = 0;
+        for (int i=0; i<6; i++) {
+            if (srcMap.getFretting().containsKey(i)) {
+                if (dstMap.getFretting().containsKey(i)) {
+                    if (srcMap.getFretting().get(i) != 0 || dstMap.getFretting().get(i) != 0) {
+                        stringMatches++;
+                        int diff = dstMap.getFretting().get(i) - srcMap.getFretting().get(i);
+                        if (stringMatches == 1) {
+                            constDiff = diff;
+                        }
+                        else {
+                            if (diff != constDiff) {
+                                stringMatches = 7;
+                                break;
+                            }
+                        }
+                    }
+                }
+                else {
+                    stringMatches = 7;
+                    break;
+                }
+            }
+            else if (dstMap.getFretting().containsKey(i) && dstMap.getFretting().get(i) != 0) {
+                stringMatches = 7;
+                break;
+            }
+        }
+        
+        transitionScore = neckMovement + matchingFingerings + dstMap.getScore();
+        // different shape between chords penalty. rewards chord changes that keep the same shape
+        if (stringMatches == 0 || stringMatches == 7) {
+            transitionScore++;
+        }
         return transitionScore;
     }
 
