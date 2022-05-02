99e331432bb05542e84985381e6b3a9b059ed27c
Tim Page <tim.page@warwick.ac.uk>
2022-05-03 00:04:42 +0100

    Added README

--- src/main/java/com/timpage/musicXMLparserDH/music/Join.java
+++ src/main/java/com/timpage/musicXMLparserDH/music/Join.java
@@ -1,5 +1,8 @@
 package com.timpage.musicXMLparserDH.music;
 
+/**
+ * Join object - for handling ties, slides, hammer-ons, and pull-offs
+ */
 public class Join {
 
     private String joinType;
@@ -8,6 +11,13 @@ public class Join {
     private int midiPitch;
     private Join matchingJoin;
 
+    /**
+     * Join object constructor
+     * @param joinType The type of join. One of: tie, slide, hammer-on, pull-off
+     * @param num Join ID, neeeded since there can be multiple at once
+     * @param type "start" or "stop"
+     * @param midiPitch The MIDI pitch of the note this end of the join
+     */
     public Join(String joinType, int num, String type, int midiPitch) {
         this.joinType = joinType;
         this.num = num;
